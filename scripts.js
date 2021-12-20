a26c0d84f11e2f95b2557440af65d67a6c15c188
Rafa Sánchez Lucena <sanchez.lucena@gmail.com>
2021-12-20 19:22:05 +0100

    Utilizo async-await

--- scripts.js
+++ scripts.js
@@ -1,29 +1,30 @@
 const boton = document.querySelector(".boton");
 
-const mensajeSorteo = () => {
-    console.log(`Se están realizando los sorteos...`);
-};
+const numSorteos = 5000;
+const numeroElegido = 11760;
+const numeroRandom = 1000;
 
-const funcionPrincipal = async (numSorteos, numeroElegido, numeroRandom, mensajeSorteo) => {
-    let posibilidades = 1 / numeroRandom;
-    let porcentaje = posibilidades * 100;
-    console.log("Comienza el sorteo...");
-    console.log(`Tienes ${posibilidades} posibilidades de que salga premiado el número: ${numeroElegido}.`);
-    console.log(`Y el porcentaje de que salga premiado es del ${porcentaje}%`);
+
+const sorteo = ()=> {
     for (let i = 0; i < numSorteos; i++) {
         let num = Math.floor(Math.random() * numeroRandom + 1);
         if (num === numeroElegido) {
             console.log(`El número ${numeroElegido} ha salido premiado en el sorteo nº: ${i}.`);
             break;
-        } mensajeSorteo();
+        }console.log("Se están realizando los sorteos...");
     }; 
+}
+
+const funcionPrincipalAsync = async (fn) => {
+    let posibilidades = 1 / numeroRandom;
+    let porcentaje = posibilidades * 100;
+    console.log("Comienza el sorteo...");
+    console.log(`Tienes ${posibilidades} posibilidades de que salga premiado el número: ${numeroElegido}.`);
+    console.log(`Y el porcentaje de que salga premiado es del ${porcentaje}%`);
+    await fn(); 
     console.log("Los sorteos han finalizado.");
 };
 
-let numSorteos = 30000;
-let numeroElegido = 1315;
-let numeroRandom = 100000;
-
 boton.addEventListener("click", function(){
-    funcionPrincipal(numSorteos, numeroElegido, numeroRandom, mensajeSorteo);
+    funcionPrincipalAsync(sorteo);
 });
